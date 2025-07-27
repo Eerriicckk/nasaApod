63d548ce67e6b982aedaa633f4b04d59284bc5db
Eerriicckk <erickrumpel67@gmail.com>
2025-07-27 16:51:47 -0300

    refactor: container inicia quando server da reset

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,4 +2,4 @@ sudo docker stop container_nasa_apod
 sudo docker rm container_nasa_apod
 sudo docker rmi nasa_apod
 sudo docker build -t nasa_apod .
-sudo docker run -d --name container_nasa_apod -p 5173:5173 nasa_apod
+sudo docker run -d --restart=always --name container_nasa_apod -p 5173:5173 nasa_apod
