36ceb7ac395730a24c7c38dbce6f51a54554218d
Eerriicckk <erickrumpel67@gmail.com>
2025-12-10 20:18:13 -0300

    refactor: ajuste vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nasaApod",
+  base: "/",
   server: {
     host: true,
     origin: "http://0.0.0.0:8080",
