691475e7e7da6b32e08cff62668e81f5908b86e4
Eerriicckk <erickrumpel67@gmail.com>
2025-12-10 20:43:24 -0300

    refactor: votando base_route

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/nasaApod",
   server: {
     host: true,
     origin: "http://0.0.0.0:8080",
